a1175defa019a18eaad7fab02f0140898047b659
Bruno Carrara <bcarrara.dev@gmail.com>
2025-07-13 17:53:48 -0300

    collections

--- communities/communities.js
+++ communities/communities.js
@@ -1,3 +1,7 @@
+// Importação da classe Mongo do pacote meteor/mongo
+// Esta importação é necessária para criar uma nova collection do MongoDB
 import { Mongo } from 'meteor/mongo';
 
+// Criação da collection Communities
+// O parâmetro 'communities' define o nome da collection no MongoDB
 export const Communities = new Mongo.Collection('communities');
--- people/people.js
+++ people/people.js
@@ -1,3 +1,7 @@
+// Importação da classe Mongo do pacote meteor/mongo
+// Esta importação é necessária para criar uma nova collection do MongoDB
 import { Mongo } from 'meteor/mongo';
 
+// Criação da collection People
+// O parâmetro 'people' define o nome da collection no MongoDB
 export const People = new Mongo.Collection('people');
